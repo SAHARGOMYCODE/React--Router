9196c89acb24c03805359683175d8269bfbd11b5
souroursahar2020 <sahar.kachoukh@gmail.com>
2020-10-31 16:44:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Film from "./Component/Film";
 import BondeAnnonce from "./Component/BondeAnnonce";
+
 import Description from "./Component/Description";
 
 function App() {
--- src/Component/BondeAnnonce.js
+++ src/Component/BondeAnnonce.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Bondeannonce extends Component {
-  render() {
-    return (
-      <div>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/S5qJXYNNINo"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      </div>
-    );
-  }
+export default function Bondeannonce() {
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/S5qJXYNNINo"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
